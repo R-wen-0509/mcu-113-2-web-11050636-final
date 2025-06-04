e74bd6f695d1d24ed0368ede90e4bd98c4961a8f
Howard <shih9305@gmail.com>
2025-06-05 00:45:09 +0800

    fix(product): 修正程式以符合 json-server 的版本要求

--- src/app/services/product-remote.service.ts
+++ src/app/services/product-remote.service.ts
@@ -18,11 +18,23 @@ export class ProductRemoteService extends ProductService {
   }
 
   override getList(name: string | undefined, index: number, size: number): Observable<{ data: Product[]; count: number }> {
-    let query = { _page: index, _per_page: size } as { name?: string; _page: number; _per_page: number };
+    let query = { _page: index, _limit: size } as { name?: string; _page: number; _limit: number };
     if (name) query = { ...query, name };
     const params = new HttpParams({ fromObject: query });
+
     return this.httpClient
-      .get<{ data: Product[]; items: number }>(this.url, { params })
-      .pipe(map(({ data, items: count }) => ({ data, count })));
+      .get<Product[]>(this.url, {
+        params,
+        observe: 'response',
+      })
+      .pipe(
+        map((response) => {
+          const count = Number(response.headers.get('x-total-count') ?? '0');
+          return {
+            data: response.body ?? [],
+            count,
+          };
+        })
+      );
   }
 }
