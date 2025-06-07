03deef8b272448f31bec1c656348bcb64735dca2
Howard <shih9305@gmail.com>
2025-06-07 14:37:21 +0800

    feat(shopping-cart): 實作從產品明細頁面加入購物車

--- src/app/product-detail-page/product-detail-page.component.ts
+++ src/app/product-detail-page/product-detail-page.component.ts
@@ -1,6 +1,7 @@
 import { CurrencyPipe } from '@angular/common';
 import { Component, inject, input, model } from '@angular/core';
 import { Router } from '@angular/router';
+import { ShoppingCartService } from '../services/shopping-cart.service';
 import { Product } from '../model/product';
 
 @Component({
@@ -16,7 +17,13 @@ export class ProductDetailPageComponent {
 
   readonly discount = model.required<boolean>();
 
+  readonly shoppingCartService = inject(ShoppingCartService);
+
   onBack(): void {
     this.router.navigate(['products']);
+  }
+
+  addToCart(product: Product): void {
+    this.shoppingCartService.addProduct(product);
   }
 }
