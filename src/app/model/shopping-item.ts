0e6f7a938f49179b1298af2c480a4e3834b762e2
Howard <shih9305@gmail.com>
2025-06-07 13:54:14 +0800

    feat(shopping-item): 新增 shopping-item 類別屬性

--- src/app/model/shopping-item.ts
+++ src/app/model/shopping-item.ts
@@ -1,2 +1,13 @@
+import { Product } from './product';
+
 export class ShoppingItem {
+  constructor(initData?: Partial<ShoppingItem>) {
+    Object.assign(this, initData);
+  }
+
+  id!: number;
+
+  count!: number;
+
+  product!: Product;
 }
